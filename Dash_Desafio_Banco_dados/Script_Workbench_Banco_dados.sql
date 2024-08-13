e2666c479f84bfeecda4711981fea741d86a4462
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2024-08-13 14:42:45 -0300

    Criando Table Employee
    Criando e configurando tabela Employee

--- Dash_Desafio_Banco_dados/Script_Workbench_Banco_dados.sql
+++ Dash_Desafio_Banco_dados/Script_Workbench_Banco_dados.sql
@@ -2,3 +2,30 @@ create schema if not exists azure_company;
 use azure_company;
 select * from information_schema.table_constraints
 	where constraint_schema = 'azure_company';
+
+-- criando tabelas
+CREATE TABLE employee(
+	Fname varchar(15) not null,
+    Minit char,
+    Lname varchar(15) not null,
+    Ssn char(9) not null, 
+    Bdate date,
+    Address varchar(30),
+    Sex char,
+    Salary decimal(10,2),
+    Super_ssn char(9),
+    Dno int not null,
+    constraint chk_salary_employee check (Salary> 2000.0),
+    constraint pk_employee primary key (Ssn)
+);
+
+alter table employee 
+	add constraint fk_employee 
+	foreign key(Super_ssn) references employee(Ssn)
+    on delete set null
+    on update cascade;
+    
+    alter table employee modify Dno int not null default 1;
+    
+desc employee;
+select * FROM employee;
