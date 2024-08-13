1f85a60145d45ba8e091443ac9a3fc86f5b69dc0
Murilo Farias <79143042+murilofarias10@users.noreply.github.com>
2024-08-13 16:02:01 -0300

    Populando tabelas: dependet, departament, dept_locations, project e works_on
    inserindo dados nas tabelas: dependet, departament, dept_locations, project, works_on

--- Dash_Desafio_Banco_dados/Script_Workbench_Banco_dados.sql
+++ Dash_Desafio_Banco_dados/Script_Workbench_Banco_dados.sql
@@ -79,9 +79,7 @@ create table dependent(
     constraint fk_dependent foreign key (Essn) references employee(Ssn)
 );
 
-show tables;
-desc employee;
-select * FROM employee;
+
 
 -- Deve seguir a ordem para inserir:
 -- Dessa forma, os supervisores estarão sempre inseridos antes dos subordinados, evitando o erro de integridade referencial.
@@ -108,5 +106,51 @@ VALUES
 
 
 
+-- inserindo dados tabelas dependet, departament, dept_locations, project, works_on
+insert into dependent values (333445555, 'Alice', 'F', '1986-04-05', 'Daughter'),
+							 (333445555, 'Theodore', 'M', '1983-10-25', 'Son'),
+                             (333445555, 'Joy', 'F', '1958-05-03', 'Spouse'),
+                             (987654321, 'Abner', 'M', '1942-02-28', 'Spouse'),
+                             (123456789, 'Michael', 'M', '1988-01-04', 'Son'),
+                             (123456789, 'Alice', 'F', '1988-12-30', 'Daughter'),
+                             (123456789, 'Elizabeth', 'F', '1967-05-05', 'Spouse');
+
+insert into departament values ('Research', 5, 333445555, '1988-05-22','1986-05-22'),
+							   ('Administration', 4, 987654321, '1995-01-01','1994-01-01'),
+                               ('Headquarters', 1, 888665555,'1981-06-19','1980-06-19');
+
+insert into dept_locations values (1, 'Houston'),
+								 (4, 'Stafford'),
+                                 (5, 'Bellaire'),
+                                 (5, 'Sugarland'),
+                                 (5, 'Houston')
+
+insert into project values ('ProductX', 1, 'Bellaire', 5),
+						   ('ProductY', 2, 'Sugarland', 5),
+						   ('ProductZ', 3, 'Houston', 5),
+                           ('Computerization', 10, 'Stafford', 4),
+                           ('Reorganization', 20, 'Houston', 1),
+                           ('Newbenefits', 30, 'Stafford', 4)
+
+insert into works_on values (123456789, 1, 32.5),
+							(123456789, 2, 7.5),
+                            (666884444, 3, 40.0),
+                            (453453453, 1, 20.0),
+                            (453453453, 2, 20.0),
+                            (333445555, 2, 10.0),
+                            (333445555, 3, 10.0),
+                            (333445555, 10, 10.0),
+                            (333445555, 20, 10.0),
+                            (999887777, 30, 30.0),
+                            (999887777, 10, 10.0),
+                            (987987987, 10, 35.0),
+                            (987987987, 30, 5.0),
+                            (987654321, 30, 20.0),
+                            (987654321, 20, 15.0),
+                            (888665555, 20, 0.0);
+                            
 
 
+show tables;
+desc works_on;
+select * FROM dept_locations;
